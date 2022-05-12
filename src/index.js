bcc80279803b05d7f9e3e189c153a487836acf92
Leo <lyuholu@gmail.com>
2022-05-12 19:11:28 +0800

    Game

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
+function Column(props) {
 
+}
+class Board extends React.Component {
 
-  // ========================================
+}
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
+class Game extends React.Component {
+    render () {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
