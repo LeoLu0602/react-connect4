3698b5557130dfb07d00f889dea96954de120e0a
Leo <lyuholu@gmail.com>
2022-05-12 19:51:12 +0800

    render buttons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 
 function Column(props) {
-
+    return (
+        <button className="column">
+            {props.value}
+        </button>
+    );
 }
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        };
+    }
+
+    renderColumn(i) {
+        return (
+            <Column 
+                value={i}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderColumn(0)}
+                {this.renderColumn(1)}
+                {this.renderColumn(2)}
+                {this.renderColumn(3)}
+                {this.renderColumn(4)}
+                {this.renderColumn(5)}
+                {this.renderColumn(6)}
+            </div>
+        );
+    }
 }
 
 class Game extends React.Component {
